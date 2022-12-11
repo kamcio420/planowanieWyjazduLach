ea136e3bfc00d0eb6be94d62513670b529de6149
kamillachdev <kamillachdev@gmail.com>
2022-12-11 13:14:05 +0100

    Declarations

--- app/src/main/java/com/example/planowaniewyjazdulach/MainActivity.kt
+++ app/src/main/java/com/example/planowaniewyjazdulach/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.planowaniewyjazdulach
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
+import android.widget.Button
+import android.widget.CalendarView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,8 +12,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupHyperlink()
+
+        //DECLARATIONS
+        val calendar = findViewById<CalendarView>(R.id.calendarView)
+        val startButton = findViewById<Button>(R.id.setTripStartButton)
+        val endButton = findViewById<Button>(R.id.setTripEndButton)
+        val confirmButton = findViewById<Button>(R.id.sumDateButton)
+        val outputText = findViewById<TextView>(R.id.tripLengthOutput)
+
     }
 
+    //LINK ACTION
     private fun setupHyperlink() {
         val linkTextView = findViewById<TextView>(R.id.activity_main_link)
         linkTextView.setMovementMethod(LinkMovementMethod.getInstance())
