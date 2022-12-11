609f8df3ffb258dd21bf3f73bb16aef0781fabc9
kamillachdev <kamillachdev@gmail.com>
2022-12-11 13:08:19 +0100

    Link action

--- app/src/main/java/com/example/planowaniewyjazdulach/MainActivity.kt
+++ app/src/main/java/com/example/planowaniewyjazdulach/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.planowaniewyjazdulach
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupHyperlink()
+    }
+
+    private fun setupHyperlink() {
+        val linkTextView = findViewById<TextView>(R.id.activity_main_link)
+        linkTextView.setMovementMethod(LinkMovementMethod.getInstance())
     }
 }
