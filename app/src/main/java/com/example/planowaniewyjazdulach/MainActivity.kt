e105819f158d466708c7c0d576516d1cf6a610c8
test <test@asd.pl>
2022-12-13 10:09:22 +0100

    confirm button prep, formatting for millis, improved calendar date

--- app/src/main/java/com/example/planowaniewyjazdulach/MainActivity.kt
+++ app/src/main/java/com/example/planowaniewyjazdulach/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.planowaniewyjazdulach
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.widget.Button
 import android.widget.CalendarView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SimpleDateFormat")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,18 +23,28 @@ class MainActivity : AppCompatActivity() {
         val confirmButton = findViewById<Button>(R.id.sumDateButton)
         val outputText = findViewById<TextView>(R.id.tripLengthOutput)
 
-        var days = 0
+        //GET CURRENT DATE
+        val pattern = SimpleDateFormat("dd/MM/yyyy")
+
+        calendar.minDate = System.currentTimeMillis()
+        calendar.maxDate = System.currentTimeMillis() + 63113904000 //2 years from current date
 
         //START BUTTON ACTION
         startButton.setOnClickListener {
-            var selectedStartDate = calendar.date
-            outputText.text = selectedStartDate.toString()
+            var formatedStartDate = pattern.format(calendar.date)
+            outputText.text = "Ustawiono date wyjazdu na:\n" + formatedStartDate
         }
 
         //END BUTTON ACTION
         endButton.setOnClickListener {
-            var selectedEndDate = calendar.date
-            outputText.text = selectedEndDate.toString()
+            var formatedEndDate = pattern.format(calendar.date)
+            outputText.text = "Ustawiono date przyjazdu na:\n" + formatedEndDate
+        }
+
+        //CONFIRM BUTTON ACTION
+        confirmButton.setOnClickListener {
+            var sumDate = " "
+            outputText.text = "Długość twojego wyjazdu :\n" + sumDate
         }
 
     }
